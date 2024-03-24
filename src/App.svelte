a8e178140ade91959a328d76ff0bcb36fa46aa0a
SlimRunner <dv.polygon@gmail.com>
2024-03-24 15:46:20 -0700

    Add shader init functions

--- src/App.svelte
+++ src/App.svelte
@@ -30,6 +30,62 @@
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
+      const shader = gl.createShader(type);
+
+      if (shader === null) {
+        throw new Error("Shader allocation failed");
+      }
+
+      // send source to the shader object
+      gl.shaderSource(shader, source);
+      gl.compileShader(shader);
+
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.warn(
+          "Shader compilation failed:",
+          gl.getShaderInfoLog(shader)
+        );
+        gl.deleteShader(shader);
+        return null;
+      }
+
+      return shader;
+    }
+
+    function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string) {
+      const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+      const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+      if (!(vertexShader && fragmentShader)) {
+        return null;
+      }
+
+      // create the shader program;
+
+      const shaderProgram = gl.createProgram();
+
+      if (shaderProgram === null) {
+        console.warn("Shader program creation failed");
+        return null;
+      }
+
+      gl.attachShader(shaderProgram, vertexShader);
+      gl.attachShader(shaderProgram, fragmentShader);
+      gl.linkProgram(shaderProgram);
+
+      if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.warn(
+          "Shader program linking failed:",
+          gl.getProgramInfoLog(shaderProgram)
+        );
+        return null;
+      }
+
+      return shaderProgram;
+    }
+
   });
 </script>
 
