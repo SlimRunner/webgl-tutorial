51ee41419c9829c68a4aba5976f0436f44a21384
SlimRunner <dv.polygon@gmail.com>
2024-03-23 17:02:56 -0700

    Set up project

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebGL Tutorial
+
+[Link to tutorial](https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial)
---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const canvas = <HTMLCanvasElement>document.querySelector("#glCanvas");
+    if (canvas === null) {
+      throw new Error("Unable to locate canvas");
+    }
+    const gl = canvas.getContext("webgl");
+
+    if (gl === null) {
+      throw new Error("Unable to initialize WebGL. Your browser or machine may not support it.");
+    }
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  });
+</script>
+
+<main>
+  <canvas id="glCanvas" width="512" height="512"></canvas>
+</main>
